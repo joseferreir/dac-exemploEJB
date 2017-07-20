176db62118c2d2e8e443175cebdfffa1e52a1766
josé <joseferreiravieira123@gmail.com>
2017-07-20 09:50:40 -0300

    first commit

---
+++ shared/src/main/java/br/edu/ifpb/dac/shared/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.dac.shared;
+
+/**
+ *
+ * @author jose2
+ */
+public interface Calculadora {
+    public int soma(int x, int y);
+    
+}
